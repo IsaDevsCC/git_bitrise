02d10f06bb95e49b8079d892b3ea6d11ebef2491
Alejandro Traver <alextfos@gmail.com>
2023-07-26 15:48:48 +0200

    add compose example

--- app/src/main/java/com/keepcoding/androidsuperpoderes/MainActivity.kt
+++ app/src/main/java/com/keepcoding/androidsuperpoderes/MainActivity.kt
@@ -3,14 +3,25 @@ package com.keepcoding.androidsuperpoderes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.keepcoding.androidsuperpoderes.ui.theme.AndroidSuperpoderesTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,32 +32,31 @@ class MainActivity : ComponentActivity() {
         setContent {
             AndroidSuperpoderesTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android", "Compose")
-                }
+                ComposableExercise()
+                /*BaseComposable {
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colorScheme.background
+                    ) {
+                        Greeting("Android", "Compose")
+                    }
+                }*/
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, lastName: String, modifier: Modifier = Modifier) {
-    Column {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
-        Text(text = lastName)
+fun ComposableExercise() {
+    Box(
+        modifier = Modifier.size(200.dp).background(Color.Green)
+    ) {
+        // Exercice content
     }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
-    AndroidSuperpoderesTheme {
-        Greeting("Android", "Compose")
-    }
+fun TestComposableExercise() {
+    ComposableExercise()
 }
