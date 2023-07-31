fa787edb04c034ff28218573c5c835d01a1168f6
Alejandro Traver <alextfos@gmail.com>
2023-07-31 20:41:08 +0200

    add range selector solution

--- app/src/test/java/com/keepcoding/androidsuperpoderes/components/RangeSelectorDelegateTest.kt
+++ app/src/test/java/com/keepcoding/androidsuperpoderes/components/RangeSelectorDelegateTest.kt
@@ -15,8 +15,34 @@ class RangeSelectorDelegateTest {
         assertThat(rangeSelectorDelegate.currentBottom, `is`(79))
     }
 
+    @Test
+    fun `WHEN moveTop EXPECT bottom is currentTop -1`() {
+        rangeSelectorDelegate.currentBottom = 50
+        rangeSelectorDelegate.moveTop(45)
+        assertThat(rangeSelectorDelegate.currentBottom, `is`(51))
+    }
 
-    // Resto de TestCases
+    @Test
+    fun `WHEN moveTop over max EXPECT top is maxValue`() {
+        rangeSelectorDelegate.moveTop(90)
+        assertThat(rangeSelectorDelegate.currentTop, `is`(80))
+    }
 
+    @Test
+    fun `WHEN moveBottom below min EXPECT currentBottom is minValue`() {
+        rangeSelectorDelegate.moveBottom(30)
+        assertThat(rangeSelectorDelegate.currentBottom, `is`(40))
+    }
 
+    @Test
+    fun `WHEN moveBottom EXPECT same value`() {
+        rangeSelectorDelegate.moveBottom(50)
+        assertThat(rangeSelectorDelegate.currentBottom, `is`(50))
+    }
+
+    @Test
+    fun `WHEN moveTop EXPECT same value`() {
+        rangeSelectorDelegate.moveBottom(50)
+        assertThat(rangeSelectorDelegate.currentBottom, `is`(50))
+    }
 }
