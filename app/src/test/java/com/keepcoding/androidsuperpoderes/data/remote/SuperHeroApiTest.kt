cf7bbe45f5487a609adb4211977ee0db64b242ba
Alejandro Traver <alextfos@gmail.com>
2023-08-01 13:08:47 +0200

    add retrofit tests

--- app/src/test/java/com/keepcoding/androidsuperpoderes/data/remote/SuperHeroApiTest.kt
+++ app/src/test/java/com/keepcoding/androidsuperpoderes/data/remote/SuperHeroApiTest.kt
@@ -1,6 +1,9 @@
 package com.keepcoding.androidsuperpoderes.data.remote
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.keepcoding.androidsuperpoderes.data.remote.dto.IdDto
+import com.keepcoding.androidsuperpoderes.data.remote.dto.LocationDto
+import com.keepcoding.androidsuperpoderes.data.remote.dto.SearchDto
 import com.keepcoding.androidsuperpoderes.di.baseUrl
 import com.keepcoding.androidsuperpoderes.testutil.DefaultDispatcherRule
 import com.squareup.moshi.Moshi
@@ -9,6 +12,9 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import org.hamcrest.CoreMatchers.instanceOf
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Before
 import org.junit.BeforeClass
 import org.junit.Rule
@@ -34,8 +40,47 @@ class SuperHeroApiTest {
 
     @Test
     fun `WHEN request hero list EXPECT result`() = runTest {
+        val result = api.getHeroList(SearchDto())
 
+        assertThat(result.isNotEmpty(), `is`(true))
     }
+
+    @Test
+    fun `WHEN request whit search EXPECT item`() = runTest {
+        val result = api.getHeroList(SearchDto("Goku"))
+
+        assertThat(result.size, `is`(1))
+    }
+
+    @Test
+    fun `WHEN request whit random text EXPECT empty list`() = runTest {
+        val result = api.getHeroList(SearchDto("asdf asdf"))
+
+        assertThat(result.isEmpty(), `is`(true))
+    }
+
+    @Test
+    fun `WHEN request hero location list EXPECT location list`() = runTest {
+        val result = api.getHeroLocationList(
+            IdDto(
+                "14BB8E98-6586-4EA7-B4D7-35D6A63F5AA3"
+            )
+        )
+
+        assertThat(result.isNotEmpty(), `is`(true))
+    }
+
+    @Test
+    fun `WHEN request hero location list EXPECT empty list`() = runTest {
+        val result = api.getHeroLocationList(
+            IdDto(
+                "81D51BD2-E82C-4E1D-8216-BDFB107C1F28"
+            )
+        )
+
+        assertThat(result, instanceOf(List::class.java))
+    }
+
 
     companion object {
         private lateinit var retrofit: Retrofit
@@ -44,6 +89,8 @@ class SuperHeroApiTest {
         @JvmStatic
         fun setupCommon() {
             retrofit = Retrofit.Builder()
+                // lo ideal es probar contra un entorno estable
+                // entorno de QA
                 .baseUrl(baseUrl)
                 .client(
                     OkHttpClient.Builder()
