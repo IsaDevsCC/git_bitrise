1ee59d3880185f763e1f5c0c1622f4f0880f57ef
Alejandro Traver <alextfos@gmail.com>
2023-08-01 14:47:46 +0200

    add first compose test

---
+++ app/src/androidTest/java/com/keepcoding/androidsuperpoderes/presentation/common/ShowHeroTest.kt
@@ -0,0 +1,35 @@
+package com.keepcoding.androidsuperpoderes.presentation.common
+
+import androidx.compose.ui.test.assertHasClickAction
+import androidx.compose.ui.test.hasClickAction
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.printToLog
+import com.keepcoding.androidsuperpoderes.HeroTestDataBuilder
+import org.junit.Rule
+import org.junit.Test
+
+class ShowHeroTest {
+    @get:Rule
+    val rule = createComposeRule()
+
+    @Test
+    fun testShowHero() {
+        rule.setContent {
+            ShowHero(
+                HeroTestDataBuilder()
+                    .withName("Sample")
+                    .buildSingle()
+            ) {}
+        }
+
+        rule.onRoot().printToLog("MY TAG")
+
+        rule.onNode(
+            hasText("Sample") and
+                    hasClickAction()
+        ).assertHasClickAction()
+
+    }
+}
